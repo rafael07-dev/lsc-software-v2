db648517c1033261dd5f333ed41043c3931bb495
rafael07-dev <deralato817@gmail.com>
2024-07-08 22:26:46 -0500

    add function to update user and create user

--- app/models/User.php
+++ app/models/User.php
@@ -18,4 +18,25 @@ class User {
         $result = $stmt->get_result();
         return $result->fetch_assoc();
     }
+
+    public function updatePassword($username, $newPassword) {
+        $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+        $query = "UPDATE users SET password = ? WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss', $hashedPassword, $username);
+        
+        if ($stmt->execute()) {
+            return true; // Actualización exitosa
+        } else {
+            return false; // Error al actualizar
+        }
+    }
+
+    public function createUser($username, $hashedPassword) {
+        $stmt = $this->db->prepare('INSERT INTO users (username, password) VALUES (?, ?)');
+        $stmt->bind_param('ss', $username, $hashedPassword);
+        $success = $stmt->execute();
+        $stmt->close();
+        return $success;
+    }
 }
