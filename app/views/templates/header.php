58d57a2bc902bb5d9a280ad4c36faff8aa5fb211
rafael07-dev <deralato817@gmail.com>
2024-07-08 22:37:13 -0500

    update links

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,11 +5,11 @@
     </div>
     <nav class="nav-list">
         <ul>
-            <li><a href="/lsc_software-v2/?page=main">Inicio</a></li>
-            <li><a href="/lsc_software-v2/?page=diccionario">Diccionario</a></li>
-            <li><a href="/lsc_software-v2/?page=aprendizaje">Aprendizaje</a></li>
-            <li><a href="/lsc_software-v2/?page=quiz">Quiz</a></li>
-            <li><a href="/lsc_software-v2/?page=perfil">Admin</a></li>
+            <li><a href="index.php?page=main">Inicio</a></li>
+            <li><a href="index.php?page=diccionario">Diccionario</a></li>
+            <li><a href="index.php?page=aprendizaje">Aprendizaje</a></li>
+            <li><a href="index.php?page=quiz">Quiz</a></li>
+            <li><a href="index.php?page=admin">Admin</a></li>
         </ul>
     </nav>
 </header>
