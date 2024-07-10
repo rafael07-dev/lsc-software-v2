e1ec8d614aa108db264082a64e2e62eb2f7eca09
rafael07-dev <deralato817@gmail.com>
2024-07-10 10:20:54 -0500

    feat: add functionality to get all words and add

--- app/models/Word.php
+++ app/models/Word.php
@@ -10,6 +10,33 @@ class Word {
         $this->db = new mysqli("localhost", "root", "", "lsc-software");
     }
 
+    public function createWord($word, $letter_id){
+        // Preparar la consulta SQL para insertar una nueva palabra
+        $query = "INSERT INTO words (word, letter_id) VALUES (?, ?)";
+        $stmt = $this->db->prepare($query);
+
+        // Vincular los parámetros
+        $stmt->bind_param("si", $word, $letter_id);
+
+        // Ejecutar la consulta
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getAllWords() {
+        $query = "SELECT * FROM words";
+        $result = $this->db->query($query);
+
+        $words = [];
+        while ($row = $result->fetch_assoc()) {
+            $words[] = $row;
+        }
+        return $words;
+    }
+
     // Método para obtener palabras que comienzan con una letra específica
     public function getWordsByLetter($letter) {
         // Primero obtener letter_id desde la letra
