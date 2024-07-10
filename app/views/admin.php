4b90cc6b4abc84025a5249597983eb29df59dbd3
rafael07-dev <deralato817@gmail.com>
2024-07-10 10:32:56 -0500

    update admin panel

--- app/views/admin.php
+++ app/views/admin.php
@@ -1,18 +1,102 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es" class="h-100">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <link rel="stylesheet" href="public/css/style.css">
-    <title>Document</title>
 </head>
-<body>
 
-<?php include 'templates/header.php' ?>
-<h1>Vista admin <?php echo $_SESSION['username'] ?></h1>
+<body class="d-flex flex-column h-100">
 
-<a href="index.php?page=admin_logout&action=logout">Cerrar sesion</a>
+    <!-- Begin page content -->
+    <main class="flex-shrink-0">
+        <div class="container">
+            <h3 class="my-3" id="titulo">Panel administrador</h3>
 
-    
+            <a href="index.php?page=admin_add_word" class="btn btn-success">Agregar</a>
+
+            <?php if (isset($data['success'])) : ?>
+                <p><?php echo $data['success']; ?></p>
+            <?php endif; ?>
+            <?php if (isset($data['error'])) : ?>
+                <p><?php echo $data['error']; ?></p>
+            <?php endif; ?>
+
+            <table class="table table-hover table-bordered my-3" aria-describedby="titulo">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Palabra</th>
+                        <th scope="col">Opciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php foreach ($data['words'] as $word) : ?>
+                        <tr>
+                            <td><?php echo $word['id'] ?></td>
+                            <td><?php echo $word['word'] ?></td>
+
+                            <td>
+                                <a href="index.php?page=admin_add_word" class="btn btn-warning btn-sm me-2">Editar</a>
+                                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#eliminaModal" data-bs-id="1">Eliminar</button>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </main>
+
+    <footer class="footer mt-auto py-3 bg-body-tertiary">
+        <div class="container">
+            <span class="text-body-secondary"> 2024 | Códigos de Programación</span>
+        </div>
+    </footer>
+
+    <div class="modal fade" id="eliminaModal" tabindex="-1" aria-labelledby="eliminaModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="eliminaModalLabel">Aviso</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>¿Desea eliminar este registro?</p>
+                </div>
+                <div class="modal-footer">
+                    <form id="form-elimina" action="" method="post">
+                        <input type="hidden" name="_method" value="delete">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <script>
+        const eliminaModal = document.getElementById('eliminaModal')
+        if (eliminaModal) {
+            eliminaModal.addEventListener('show.bs.modal', event => {
+                // Button that triggered the modal
+                const button = event.relatedTarget
+                // Extract info from data-bs-* attributes
+                const id = button.getAttribute('data-bs-id')
+
+                // Update the modal's content.
+                const form = eliminaModal.querySelector('#form-elimina')
+                form.setAttribute('action', 'elimina.html?id=' + id)
+            })
+        }
+    </script>
+
 </body>
+
 </html>
